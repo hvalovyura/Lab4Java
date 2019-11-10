928c87590f4baa142391f1d66b420dd9fbe5f414
Yura <hvalovyura@gmail.com>
2019-11-11 02:53:06 +0300

    graphicsmenulistener добавлен в класс mainframe

--- src/Lab4VarA/MainFrame.java
+++ src/Lab4VarA/MainFrame.java
@@ -1,6 +1,8 @@
 package Lab4VarA;
 
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 import java.awt.*;
 
 public class MainFrame extends JFrame
@@ -10,5 +12,25 @@ public class MainFrame extends JFrame
     public static void main(String[] args)
     {
         System.out.println("qq");
+    }
+
+    public class GraphicsMenuListener implements MenuListener
+    {
+
+        @Override
+        public void menuSelected(MenuEvent e) {
+            showAxisMenuItem.setEnabled(fileLoaded);
+            showMarkersMenuItem.setEnabled(fileLoaded);
+        }
+
+        @Override
+        public void menuDeselected(MenuEvent e) {
+
+        }
+
+        @Override
+        public void menuCanceled(MenuEvent e) {
+
+        }
     }
 }
