14e86f88dd58519f1345786653ffa0ff04fdbb3b
Yura <hvalovyura@gmail.com>
2019-11-11 01:30:04 +0300

    функция изображения графика

--- src/Lab4VarA/GraphicsDisplay.java
+++ src/Lab4VarA/GraphicsDisplay.java
@@ -2,6 +2,7 @@ package Lab4VarA;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 
 public class GraphicsDisplay extends JPanel
@@ -62,6 +63,28 @@ public class GraphicsDisplay extends JPanel
         Point2D.Double dest = new Point2D.Double();
         dest.setLocation(src.getX() + deltaX, src.getY() + deltaY);
         return dest;
+    }
+
+    protected void paintGraphics(Graphics2D canvas)
+    {
+        canvas.setStroke(graphicsStroke);
+        canvas.setColor(Color.RED);
+
+        GeneralPath graphics = new GeneralPath();
+        for(int i = 0; i < graphicsData.length; i++)
+        {
+            Point2D.Double point = xyToPoint(graphicsData[i][0], graphicsData[i][1]);
+
+            if(i > 0)
+            {
+                graphics.lineTo(point.getX(), point.getY());
+            }
+            else
+            {
+                graphics.moveTo(point.getX(), point.getY());
+            }
+        }
+        canvas.draw(graphics);
     }
 
 }
