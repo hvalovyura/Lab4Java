d3dd29e0ad6f7be5d480f91922924aff52b4a3bc
Yura <hvalovyura@gmail.com>
2019-11-11 07:12:04 +0300

    добавлено заполнение файла

--- src/Lab4VarA/MainFrame.java
+++ src/Lab4VarA/MainFrame.java
@@ -113,6 +113,19 @@ public class MainFrame extends JFrame
 
     public static void main(String[] args)
     {
+        try(DataOutputStream dos = new DataOutputStream(new FileOutputStream("x")))
+        {
+            Double[] a = {-5.1234, -5.6789, -4.789, -4.123, -3.456, -3.456, -2.0, -2.0, 0.0, 0.3, 1.234, 1.234, 2.0, 2.3, 3.0, 3.0};
+            for(int i = 0; i < a.length; i++)
+            {
+                dos.writeDouble(a[i]);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
         MainFrame frame = new MainFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
