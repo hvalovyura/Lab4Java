6b93ba35ff0e1525d99df46c60081bc7f66df9ef
Yura <hvalovyura@gmail.com>
2019-11-11 03:20:30 +0300

    конструктор главного класса дописан

--- src/Lab4VarA/MainFrame.java
+++ src/Lab4VarA/MainFrame.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class MainFrame extends JFrame
 {
@@ -18,6 +20,63 @@ public class MainFrame extends JFrame
     private GraphicsDisplay display = new GraphicsDisplay();
 
     private boolean fileLoaded = false;
+
+    public MainFrame()
+    {
+        super("Построение графиков функций на основе подготовленных файлов");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH) / 2, (kit.getScreenSize().height - HEIGHT) / 2);
+        setExtendedState(MAXIMIZED_BOTH);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        Action openGraphicsAction = new AbstractAction("Открыть файл") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (fileChooser == null)
+                {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showOpenDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION)
+                {
+                    openGraphics(fileChooser.getSelectedFile());
+                }
+            }
+        };
+        fileMenu.add(openGraphicsAction);
+
+        JMenu graphicsMenu = new JMenu("График");
+        menuBar.add(graphicsMenu);
+
+        Action showAxisAction = new AbstractAction("Показывать оси координат") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setShowAxis(showAxisMenuItem.isSelected());
+            }
+        };
+        showAxisMenuItem = new JCheckBoxMenuItem(showAxisAction);
+        graphicsMenu.add(showAxisMenuItem);
+        showAxisMenuItem.setSelected(true);
+
+        Action showMarkersAction = new AbstractAction("Показывать маркеры точек") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setShowMarkers(showMarkersMenuItem.isSelected());
+            }
+        };
+        showMarkersMenuItem = new JCheckBoxMenuItem(showMarkersAction);
+        graphicsMenu.add(showMarkersMenuItem);
+        showMarkersMenuItem.setSelected(false);
+
+        graphicsMenu.addMenuListener(new GraphicsMenuListener());
+
+        getContentPane().add(display, BorderLayout.CENTER);
+    }
 
     public static void main(String[] args)
     {
